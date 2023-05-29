8a2fca3bd1a4c575871d4be4df7783fb167f7ad8
sgrade <sgrade@users.noreply.github.com>
2023-05-29 20:22:01 +0000

    leetcode

--- leetcode/design-parking-system/design-parking-system.go
+++ leetcode/design-parking-system/design-parking-system.go
@@ -8,18 +8,12 @@ type ParkingSystem struct {
 }
 
 func Constructor(big int, medium int, small int) ParkingSystem {
-	return ParkingSystem{
-		slots: map[int]int{
-			1: big,
-			2: medium,
-			3: small,
-		},
-	}
+	return ParkingSystem{slots: map[int]int{1: big, 2: medium, 3: small}}
 }
 
 func (this *ParkingSystem) AddCar(carType int) bool {
-	if this.slots[carType] != 0 {
-		this.slots[carType] -= 1
+	if this.slots[carType] > 0 {
+		this.slots[carType]--
 		return true
 	}
 	return false
