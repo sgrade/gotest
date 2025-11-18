12756ec3d09a2a9c68c0177882956717d359ac08
sgrade <sgrade@users.noreply.github.com>
2025-11-18 11:48:21 +0000

    leetcode

---
+++ leetcode/1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.go
@@ -0,0 +1,16 @@
+package bitand2bitcharacters
+
+// 717. 1-bit and 2-bit Characters
+// https://leetcode.com/problems/1-bit-and-2-bit-characters/
+
+func isOneBitCharacter(bits []int) bool {
+	ones := 0
+	for i := len(bits) - 2; i >= 0; i-- {
+		if bits[i] == 0 {
+			ones++
+		} else {
+			break
+		}
+	}
+	return ones%2 == 0
+}
