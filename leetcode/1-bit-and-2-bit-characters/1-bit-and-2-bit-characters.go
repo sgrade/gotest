be47196cf59cfc6e96d925f3548102d7c25b6980
sgrade <sgrade@users.noreply.github.com>
2025-11-18 11:53:08 +0000

    leetcode

--- leetcode/1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.go
+++ leetcode/1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.go
@@ -6,7 +6,7 @@ package bitand2bitcharacters
 func isOneBitCharacter(bits []int) bool {
 	ones := 0
 	for i := len(bits) - 2; i >= 0; i-- {
-		if bits[i] == 0 {
+		if bits[i] == 1 {
 			ones++
 		} else {
 			break
