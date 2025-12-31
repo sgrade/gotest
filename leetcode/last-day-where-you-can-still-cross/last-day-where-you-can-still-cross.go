93839887c951823c500f0df4c3b0fcafac3eb0b2
sgrade <sgrade@users.noreply.github.com>
2025-12-31 19:13:57 +0000

    leetcode

---
+++ leetcode/last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.go
@@ -0,0 +1,92 @@
+// 2203. Last Day Where You Can Still Cross
+// https://leetcode.com/problems/last-day-where-you-can-still-cross/
+
+package lastdaywhereyoucanstillcross
+
+// Based on Editorial's Approach 3: Disjoint Set Union (on land cells)
+
+// UnionFind (Disjoint Set Union / Union-Find)
+type UnionFind struct {
+	parent []int
+	rank   []int
+}
+
+// NewUnionFind creates a new UnionFind with n elements
+func NewUnionFind(n int) *UnionFind {
+	parent := make([]int, n)
+	rank := make([]int, n)
+	for i := range parent {
+		parent[i] = i
+	}
+	return &UnionFind{parent: parent, rank: rank}
+}
+
+// Find finds the root of x with path compression
+func (uf *UnionFind) Find(x int) int {
+	if uf.parent[x] != x {
+		uf.parent[x] = uf.Find(uf.parent[x]) // path compression
+	}
+	return uf.parent[x]
+}
+
+// Union merges sets containing x and y using union by rank
+func (uf *UnionFind) Union(x, y int) {
+	rootX := uf.Find(x)
+	rootY := uf.Find(y)
+	if rootX == rootY {
+		return
+	}
+	// union by rank
+	if uf.rank[rootX] < uf.rank[rootY] {
+		uf.parent[rootX] = rootY
+	} else if uf.rank[rootX] > uf.rank[rootY] {
+		uf.parent[rootY] = rootX
+	} else {
+		uf.parent[rootY] = rootX
+		uf.rank[rootX]++
+	}
+}
+
+// IsConnected checks if x and y are in the same set
+func (uf *UnionFind) IsConnected(x, y int) bool {
+	return uf.Find(x) == uf.Find(y)
+}
+
+func latestDayToCross(row int, col int, cells [][]int) int {
+	dsu := NewUnionFind(row*col + 2)
+	grid := make([][]int, row)
+	for r := range grid {
+		grid[r] = make([]int, col)
+		for c := range grid[r] {
+			grid[r][c] = 1
+		}
+	}
+
+	directions := [][]int{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
+
+	for day := len(cells) - 1; day >= 0; day-- {
+		r := cells[day][0] - 1
+		c := cells[day][1] - 1
+		grid[r][c] = 0
+		idx1 := r*col + c + 1
+		for _, dir := range directions {
+			newR := r + dir[0]
+			newC := c + dir[1]
+			idx2 := newR*col + newC + 1
+			if newR >= 0 && newR < row && newC >= 0 && newC < col && grid[newR][newC] == 0 {
+				dsu.Union(idx1, idx2)
+			}
+		}
+		if r == 0 {
+			dsu.Union(0, idx1)
+		}
+		if r == row-1 {
+			dsu.Union(row*col+1, idx1)
+		}
+		if dsu.Find(0) == dsu.Find(row*col+1) {
+			return day
+		}
+	}
+
+	return -1
+}
