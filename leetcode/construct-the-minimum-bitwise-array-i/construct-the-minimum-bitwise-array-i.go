59c8ac5c8ec9010e16a67f114053e2f14b55bf86
sgrade <sgrade@users.noreply.github.com>
2026-01-20 11:07:30 +0000

    leetcode

---
+++ leetcode/construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.go
@@ -0,0 +1,17 @@
+// 2655. Construct the Minimum Bitwise Array I
+// https://leetcode.com/problems/construct-the-minimum-bitwise-array-i/
+
+package constructtheminimumbitwisearrayi
+
+func minBitwiseArray(nums []int) []int {
+	for i, num := range nums {
+		cur := -1
+		bit := 1
+		for (num & bit) != 0 {
+			cur = num - bit
+			bit <<= 1
+		}
+		nums[i] = cur
+	}
+	return nums
+}
