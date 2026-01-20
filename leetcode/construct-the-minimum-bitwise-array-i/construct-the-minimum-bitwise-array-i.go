700ba3d1b9c19d9c6f2f069af7f99c1831c1d261
sgrade <sgrade@users.noreply.github.com>
2026-01-20 11:08:14 +0000

    leetcode

--- leetcode/construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.go
+++ leetcode/construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.go
@@ -1,4 +1,4 @@
-// 2655. Construct the Minimum Bitwise Array I
+// 3314. Construct the Minimum Bitwise Array I
 // https://leetcode.com/problems/construct-the-minimum-bitwise-array-i/
 
 package constructtheminimumbitwisearrayi
