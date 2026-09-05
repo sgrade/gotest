4fbe4d9e69b7e9b606ec27c5352643b124628cd3
sgrade <sgrade@users.noreply.github.com>
2026-09-05 12:25:09 +0000

    leetcode

---
+++ leetcode/smallest-stable-index-ii/smallest-stable-index-ii.go
@@ -0,0 +1,25 @@
+// 3904. Smallest Stable Index II
+// https://leetcode.com/problems/smallest-stable-index-ii/
+
+package smalleststableindexii
+
+func firstStableIndex(nums []int, k int) int {
+	n := len(nums)
+
+	// suffixMin[i] is the minimum of nums[i:].
+	suffixMin := make([]int, n)
+	suffixMin[n-1] = nums[n-1]
+	for i := n - 2; i >= 0; i-- {
+		suffixMin[i] = min(nums[i], suffixMin[i+1])
+	}
+
+	// Sweep left to right; prefixMax is the maximum of nums[:i+1].
+	prefixMax := nums[0]
+	for i, num := range nums {
+		prefixMax = max(prefixMax, num)
+		if prefixMax-suffixMin[i] <= k {
+			return i
+		}
+	}
+	return -1
+}
